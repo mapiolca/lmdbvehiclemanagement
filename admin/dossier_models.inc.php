c9160b7d0f1d21a19759a0bd7cdc6166927631b3
Pierre Ardoin <32256817+mapiolca@users.noreply.github.com>
2026-09-04 21:22:49 +0200

    Use native document-model title translation

--- admin/dossier_models.inc.php
+++ admin/dossier_models.inc.php
@@ -8,7 +8,7 @@ require_once DOL_DOCUMENT_ROOT.'/core/lib/functions2.lib.php';
  */
 $dossierModels = getListOfModels($db, 'lmdbvehicle');
 $dossierActive = is_array($dossierModels) && isset($dossierModels['lmdb_vehicle_dossier']);
-print load_fiche_titre($langs->trans('DocumentModels'), '', 'pdf');
+print load_fiche_titre($langs->trans('DocumentModules'), '', 'pdf');
 print '<div class="div-table-responsive-no-min"><table class="noborder centpercent"><tr class="liste_titre"><th>'.$langs->trans('Name').'</th><th>'.$langs->trans('Description').'</th><th class="center">'.$langs->trans('Status').'</th><th class="center">'.$langs->trans('Default').'</th></tr>';
 print '<tr class="oddeven"><td>'.$langs->trans('LmdbVehicleDossier').'</td><td>'.$langs->trans('LmdbVehicleDossierDescription').'</td><td class="center">';
 print '<a href="'.$_SERVER['PHP_SELF'].'?action='.($dossierActive ? 'disable_dossier' : 'enable_dossier').'&token='.newToken().'">'.img_picto($langs->trans($dossierActive ? 'Disable' : 'Activate'), $dossierActive ? 'switch_on' : 'switch_off').'</a></td><td class="center">';
